5047bee4e79f43889c561f004db8b3d42f01fd48
Garima Bhanot <garima.bhanot@nagarro.com>
2023-08-13 19:58:44 +0530

    modified test class

--- src/test/java/TestCases/TestClass.java
+++ src/test/java/TestCases/TestClass.java
@@ -69,5 +69,18 @@ public class TestClass {
 		System.out.println("product is "+product);
 		Assert.assertTrue(product>0);
 	}
+	/**
+	 * This test method 
+	 * checks whether given 2 strings
+	 * are equal
+	 */
+	@Test
+	public void Test04_VerifyGivenStringsAreEqual() {
+		
+		String first="thisisfirst";
+		String second="thisisSecond";
+		Assert.assertEquals(first, second,"Assert Failed : Given strings are not equal");
+	}
+	
 	
 }
